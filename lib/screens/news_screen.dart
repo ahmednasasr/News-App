4474b9af79367eb605ec757274d098082926f25b
ahmednassar <ahmednassar863@gmail.com>
2024-09-09 04:16:23 +0300

    third push we add cashed network image

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp/core/services/apis/api_manger.dart';
 import 'package:newsapp/models/news_model.dart';
@@ -66,14 +67,37 @@ class _NewsScreenState extends State<NewsScreen> {
                         }else{
                           List<Article> article = snapshot.data?.articles??[];
                           return Expanded(
-                            child: ListView.builder(
-                              itemCount: article.length,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Image.network(article[index].urlToImage),
-                                );
-                              },),
+                              child: ListView.builder(
+                                itemCount: article.length,
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          ClipRRect(
+                                              borderRadius: BorderRadius.circular(10),
+                                            child:CachedNetworkImage(
+                                            imageUrl: article[index].urlToImage??"",
+                                            placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+                                            errorWidget: (context, url, error) => Center(child: Icon(Icons.error)),
+                                          ),
+                                    ),
+                                      Text(article[index].source.name??""),
+                                      Text(article[index].title??""),
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.end,
+                                            children: [
+                                              Text(article[index].publishedAt.substring(0,10)??"")
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },),
+                            
                           );
                         }
                       },),
